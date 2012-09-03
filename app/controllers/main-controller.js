1fffed229abf085a9e24df4332c58c452d1c9480
Sergio Almecija <sheniff@gmail.com>
2012-09-03 01:51:30 -0700

    Register via Facebook, Twitter or Password

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -3,7 +3,7 @@ module.exports = function(app, auth){
 
   // home
   app.get('/', function(req, res){
-    //res.redirect('/articles')
+
     res.render('main', {
       title: 'Main'
     })
--- app/models/user.js
+++ app/models/user.js
@@ -26,7 +26,7 @@ everyauth.debug = true
 /// FACEBOOK CONFIG PARAMS
 
 // This is how you request permissions
-everyauth.facebook.scope('email, user_about_me, user_location')
+everyauth.facebook.scope('email, user_about_me')
 
 // Eleminate timeout completely
 everyauth.facebook.moduleTimeout(-1)
@@ -34,135 +34,168 @@ everyauth.facebook.moduleTimeout(-1)
 // To see all the configurable options
 // console.log(everyauth.facebook.configurable())
 
-/// TWITTER CONFIG PARAMS
+// everyauth.everymodule.findUserById( function(userId, callback){
 
+// });
 
-/// LOGIN & PASS CONFIG PARAMS
-
+var AccountType = "free premium superadmin".split(" ");
 
 UserSchema.add({
-    bio: String
-  , location: {
-      name: String
-    }
-  , fbProfileUrl: String
-  , created_at  : {type : Date, default : Date.now}
-})
+  email: String,
+  name: {
+    first: String, 
+    last: String
+  },
+  account_type: {type: String, enum: AccountType, default: "free"},
+  bio: String,
+  created_at: {type : Date, default : Date.now}
+});
+
+
+// everyauth.everymodule.findUserById (function(id,callback){
+//   console.log("AAAAAAAAAAAAAAGGHHHH ", id);
+//   User.findById(id, function(err, user) {
+//     if(err) return callback(err);
+//     callback(null, user);
+//   });
+// });
 
 
 UserSchema.plugin(mongooseAuth, {
-    everymodule: {
-      everyauth: {
-        User: function () {
-          return User
-        }
+  everymodule: {
+    everyauth: {
+      User: function() {
+        return User;
+      },
+      //Method rewriting to work properly with mongoose!!
+      findUserById: function (userId, callback) {
+        User.findById(userId, function(err, user){
+          if(err) return callback(err);
+          callback(null,user);
+        });
       }
     }
-  , facebook: {
-      everyauth: {
-          myHostname: config.facebook.host_uri
-        , appId: config.facebook.appId
-        , appSecret: config.facebook.appSecret
-        , redirectPath: '/'
-        , findOrCreateUser: function (sess, accessTok, accessTokExtra, fbUser) {
-            var promise = this.Promise()
-              , User = this.User()()
-            // TODO Check user in session or request helper first
-            //      e.g., req.user or sess.auth.userId
-            User.findOne({'fb.id': fbUser.id}, function (err, foundUser) {
-              if (err) return promise.fail(err)
-              if (foundUser) {
-                return promise.fulfill(foundUser)
+  },
+  password: {
+    loginWith: 'email',
+    everyauth: {
+      getLoginPath: '/login',
+      postLoginPath: '/login',
+      loginView: 'users/login.jade',
+      loginSuccessRedirect: '/',
+      loginLocals: {
+        appName: "Login",
+        title: "PhotoHub"
+      },
+      getRegisterPath: '/register',
+      postRegisterPath: '/register',
+      registerView: 'users/register.jade',
+      registerSuccessRedirect: '/',
+      registerLocals: {
+        appName: "Register",
+        title: "PhotoHub"
+      },
+    }
+  },
+  facebook: {
+    everyauth: {
+      myHostname: config.facebook.host_uri,
+      appId: config.facebook.appId,
+      appSecret: config.facebook.appSecret,
+      redirectPath: '/',
+      findOrCreateUser: function (session, accessTok, accessTokExtra, fbUser) {
+        var promise = this.Promise(),
+          User = this.User()();
+
+        User.findById(session.auth.userId, function (err, foundUser) {
+          if (err) return promise.fail(err);          
+          if (!foundUser) {
+            User.where('password.email', fbUser.mail).findOne( function (err, user) {
+              if (err) return promise.fail(err);
+              if (!user) {
+                User.createWithFB(fbUser, accessTok, accessTokExtra.expires, function (err, createdUser) {
+                  if (err) return promise.fail(err);
+                  return promise.fulfill(createdUser);
+                }); 
+              } else {
+                assignFbDataToUser(user, accessTok, accessTokExtra, fbUser);
+                user.save( function (err, user) {
+                  if (err) return promise.fail(err);
+                  promise.fulfill(user);
+                });
               }
-              
-              console.log("Not found... CREATING")
-
-              var expiresDate = new Date
-              expiresDate.setSeconds(expiresDate.getSeconds() + accessTokExtra)
-
-              user = new User({
-                  fb: {
-                      id: fbUser.id
-                    , accessToken: accessTok
-                    , expires: expiresDate
-                    , name: {
-                          full: fbUser.name
-                        , first: fbUser.first_name
-                        , last: fbUser.last_name
-                      }
-                    , alias: fbUser.link.match(/^http:\/\/www.facebook\.com\/(.+)/)[1]
-                    , gender: fbUser.gender
-                    , email: fbUser.email
-                    , timezone: fbUser.timezone
-                    , locale: fbUser.locale
-                    , updatedTime: fbUser.updated_time
-                  }
-                , fbProfileUrl: fbUser.link
-                , bio: fbUser.bio
-                , location: {
-                    name: fbUser.location && fbUser.location.name ? fbUser.location.name : ''
-                  }
-              })
-
-              user.save( function (err, savedUser) {
-                promise.fulfill(savedUser)
-              })
-            })
-
-            return promise
+            });
+          } else {
+            assignFbDataToUser(user, accessTok, accessTokExtra, fbUser);
+            user.save( function (err, user) {
+              if (err) return promise.fail(err);
+              promise.fulfill(user);
+            });
           }
+        });
+        return promise;
       }
     }
-  , twitter: {
-      everyauth: {
-        , consumerKey: config.twitter.consumerKey
-        , consumerSecret: config.twitter.consumerSecret
-        , redirectPath: '/'
-        , findOrCreateUser: function (sess, accessToken, accessSecret, twitUser) {
-            var promise = this.Promise()
-              , User = this.User()()
-            User.findOne({'tw.id': twitUser.id}, function (err, foundUser) {
-              if (err) return promise.fail(err)
-              if (foundUser) {
-                return promise.fulfill(foundUser)
+  },
+  twitter: {
+    everyauth: {
+      consumerKey: config.twitter.consumerKey,
+      consumerSecret: config.twitter.consumerSecret,
+      redirectPath: '/',
+      findOrCreateUser: function (session, accessToken, accessTokenSecret, twUser) {
+        var promise = this.Promise(),
+          User = this.User()();
+
+        User.findById(session.auth.userId, function (err, foundUser) {
+          if (err) return promise.fail(err);
+          if (!foundUser) {
+            User.where('password.email', twUser.mail).findOne( function (err, user) {
+              if (err) return promise.fail(err);
+              if (!user) {
+                User.createWithTW(twUser, accessToken, accessTokenSecret, function (err, createdUser) {
+                  if (err) return promise.fail(err);
+                  return promise.fulfill(createdUser);
+                }); 
+              } else {
+                assignTwDataToUser(user, accessToken, accessTokenSecret, twUser);
+                user.save( function (err, user) {
+                  if (err) return promise.fail(err);
+                  promise.fulfill(user);
+                });
               }
-              console.log("CREATING")
-
-              user = new User({
-                  tw: {
-                      id: twitUser.id
-                    , accessToken: accessToken
-                    , accessSecret: accessSecret
-                    , expires: expiresDate
-                    , name: {
-                          full: twitUser.name
-                        , first: twitUser.first_name
-                        , last: twitUser.last_name
-                      }
-                    , alias: twitUser.link.match(/^http:\/\/www.facebook\.com\/(.+)/)[1]
-                    , gender: fbUser.gender
-                    , email: fbUser.email
-                    , timezone: fbUser.timezone
-                    , locale: fbUser.locale
-                    , updatedTime: fbUser.updated_time
-                  }
-                , twProfileUrl: twitUser.link
-                , bio: twitUser.bio
-                , location: {
-                    name: twitUser.location && twitUser.location.name ? twitUser.location.name : ''
-                  }
-              })
-
-              user.save( function (err, savedUser) {
-                promise.fulfill(savedUser)
-              })
-            })
-
-            return promise
+            });
+          } else {
+            assignTwDataToUser(foundUser, accessToken, accessTokenSecret, twUser);
+            user.save( function (err, user) {
+              if (err) return promise.fail(err);
+              promise.fulfill(user);
+            });
           }
-      }      
+        });
+
+        return promise;
+      }
     }
-})
+  }
+});
+
+//////////////////
+// Extra functions
+//////////////////
+
+var assignFbDataToUser = function (user, accessTok, accessTokExtra, fbUser) {
+  user.fb.accessToken = accessTok;
+  user.fb.expires = accessTokExtra.expires;
+  user.fb.id = fbUser.id;
+  user.fb.name = fbUser.name;
+};
+
+var assignTwDataToUser = function (user, accessTok, accessTokSecret, twUser) {
+  user.twit.accessToken = accessTok;
+  user.twit.accessTokenSecret = accessTokSecret;
+  user.twit.id = twUser.id;
+  user.twit.name = twUser.screen_name;
+};
 
 // validations
 
@@ -174,75 +207,6 @@ UserSchema.path('fb.email').validate(function (email) {
   return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i.test(email)
 }, 'Please provide a proper email')
 
-/**************************/
-/**** USERNAME & PASS *****/
-/**************************/
-
-/*
-everyauth
-  .password
-    .loginWith('email')
-    .getLoginPath('/login')
-    .postLoginPath('/login')
-    .loginView('login.jade')
-//    .loginLocals({
-//      title: 'Login'
-//    })
-//    .loginLocals(function (req, res) {
-//      return {
-//        title: 'Login'
-//      }
-//    })
-    .loginLocals( function (req, res, done) {
-      setTimeout( function () {
-        done(null, {
-          title: 'Async login'
-        });
-      }, 200);
-    })
-    .authenticate( function (login, password) {
-      var errors = [];
-      if (!login) errors.push('Missing login');
-      if (!password) errors.push('Missing password');
-      if (errors.length) return errors;
-      var user = usersByLogin[login];
-      if (!user) return ['Login failed'];
-      if (user.password !== password) return ['Login failed'];
-      return user;
-    })
-
-    .getRegisterPath('/register')
-    .postRegisterPath('/register')
-    .registerView('register.jade')
-//    .registerLocals({
-//      title: 'Register'
-//    })
-//    .registerLocals(function (req, res) {
-//      return {
-//        title: 'Sync Register'
-//      }
-//    })
-    .registerLocals( function (req, res, done) {
-      setTimeout( function () {
-        done(null, {
-          title: 'Async Register'
-        });
-      }, 200);
-    })
-    .validateRegistration( function (newUserAttrs, errors) {
-      var login = newUserAttrs.login;
-      if (usersByLogin[login]) errors.push('Login already taken');
-      return errors;
-    })
-    .registerUser( function (newUserAttrs) {
-      var login = newUserAttrs[this.loginKey()];
-      return usersByLogin[login] = addUser(newUserAttrs);
-    })
-
-    .loginSuccessRedirect('/')
-    .registerSuccessRedirect('/');
-*/
-
 // virtual attributes
 
-var exports = module.exports = User = mongoose.model('User', UserSchema)
+var exports = module.exports = User = mongoose.model('User', UserSchema);
